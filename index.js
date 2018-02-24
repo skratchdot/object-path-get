ef101c165842d01685e5926e5ec5b1407f123b53
skratchdot <git@skratchdot.com>
2018-02-24 13:25:48 -0500

    add prettier and `npm run build`

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 
-module.exports = exports = function (obj, path, defaultValue, delimiter) {
-	if (typeof path === 'string') {
-		path = path.split(delimiter || '.');
-	}
-	if (Array.isArray(path)) {
-		var len = path.length;
-		for (var i = 0; i < len; i++) {
-			if (obj && (obj.hasOwnProperty(path[i]) || obj[path[i]])) {
-				obj = obj[path[i]];
-			} else {
-				return defaultValue;
-			}
-		}
-		return obj;
-	} else {
-		return defaultValue;
-	}
+module.exports = exports = function(obj, path, defaultValue, delimiter) {
+  if (typeof path === 'string') {
+    path = path.split(delimiter || '.');
+  }
+  if (Array.isArray(path)) {
+    var len = path.length;
+    for (var i = 0; i < len; i++) {
+      if (obj && (obj.hasOwnProperty(path[i]) || obj[path[i]])) {
+        obj = obj[path[i]];
+      } else {
+        return defaultValue;
+      }
+    }
+    return obj;
+  } else {
+    return defaultValue;
+  }
 };
