dab454b75d18070159f3fe5c1cb495a385903883
Pravdomil <pravdomil.toman@gmail.com>
2018-02-26 10:03:03 +0100

    pass tests please

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-module.exports = function(obj, path, defaultValue, delimeter) {
+"use strict";
+
+module.exports = function(obj, path, defaultValue, delimeter = ".") {
     let found = false
     
-    for (const segment of path.split(delimiter || ".")) {
+    for (const segment of path.split(delimiter)) {
         if (typeof obj !== "object" || obj === null || !(segment in obj)) {
             found = false
             break
